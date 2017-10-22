483374ae52435f1909b54348daf0daa6e5bbeff4
sarahmluo <sarah.mcbride925@gmail.com>
2017-10-22 19:54:57 -0400

    Adding Assignment 8

---
+++ Assignment 8/Assignment8/Scripts/Home.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+    $("button").click(function () {
+
+        var playerNumber = $(".player-number-textbox-input").val();
+        //alert(playerNumber);
+        
+        $.ajax({
+            url: "Home/GetPlayerInformation",
+            data: { PlayerNumber: playerNumber },
+            success: function (data) {
+                var player = JSON.parse(data);
+                $(".output").text(player.PlayerName);
+
+            }
+        });
+
+       // $.get("HomeController/GetPlayerInformation", { PlayerNumber: playerNumber }, function (data) { alert(data);})
+        
+    });
+
+});
